746aa344914e94c19fd15c688a049e5455c93a3b
thecil <thecilsurf@gmail.com>
2023-12-15 21:17:22 -0400

    Update page.tsx

--- app/address/[account]/page.tsx
+++ app/address/[account]/page.tsx
@@ -15,7 +15,7 @@ export const generateMetadata = ({ params }: Props): Metadata => {
 
 const Page = ({ params }: Props) => {
   return (
-    <div className="p-4 min-h-screen md:mx-auto md:w-9/12 w-full">
+    <div className="p-4 min-h-screen md:mx-auto md:w-9/12">
       {/* header*/}
       <div className="py-4 border-b border-gray-500 ">
         <div className="flex items-center space-x-2">
