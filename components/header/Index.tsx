bb0256e22b1f9e0139a90535f7f2ce35c6971e5b
thecil <thecilsurf@gmail.com>
2024-03-01 18:05:00 -0400

    Update Index.tsx

--- components/header/Index.tsx
+++ components/header/Index.tsx
@@ -9,15 +9,17 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 const Header: React.FC = () => {
   const pathname = usePathname();
   return (
-    <header className="p-2 md:px-20 flex justify-between items-center border-b">
-      <Link className="lg:text-2xl font-bold" href="/">
-        thecil - Block Explorer
-      </Link>
-      {pathname !== "/" ? <Search className="w-1/3"/> : null}
-      <div className="flex space-x-2 items-center">
-        <ConnectButton />
-        <Separator className="h-10" orientation="vertical" />
-        <HeaderMenu />
+    <header className="p-4 border-b">
+      <div className="container flex justify-between items-center ">
+        <Link className="lg:text-2xl font-bold" href="/">
+          thecil - Block Explorer
+        </Link>
+        {pathname !== "/" ? <Search className="w-1/3" /> : null}
+        <div className="flex space-x-2 items-center">
+          <ConnectButton />
+          <Separator className="h-10" orientation="vertical" />
+          <HeaderMenu />
+        </div>
       </div>
     </header>
   );
