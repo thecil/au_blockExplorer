70e1024828eb408f2686441db99c0c948ffbd1b6
thecil <thecilsurf@gmail.com>
2024-03-21 01:47:19 -0400

    ref: latest block , add stage error

--- components/web3/LatestBlockNumber.tsx
+++ components/web3/LatestBlockNumber.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useBlockQuery } from "@/queries/block-query";
 import Loading from "../Loading";
 import TooltipController from "../ToolTipController";
+import { Stages } from "@/types/components";
 const LatestBlockNumber: React.FC = () => {
+  const [stage, setStage] = useState(Stages.loading);
   const { latestBlockQuery } = useBlockQuery();
   const {
     data: blockNumber,
@@ -15,29 +17,50 @@ const LatestBlockNumber: React.FC = () => {
     isStale
   } = latestBlockQuery;
 
-  if (isLoading || isRefetching) return <Loading />;
-  if (error) return <ErrorData error={error} />;
-  if (blockNumber)
-    return (
-      <TooltipController
-        content="Latest block number minded every 12 secs"
-        side="bottom"
-      >
-        <div className="flex space-x-1 items-center">
-          <div
-            className={`w-3 h-3 ${
-              isStale ? "bg-yellow-600" : "bg-green-600"
-            } rounded-full`}
-          ></div>
+  useEffect(() => {
+    if (isLoading || isRefetching) {
+      if (stage !== Stages.loading) {
+        setStage(Stages.loading);
+        return;
+      }
+    }
+    if (error) {
+      if (stage !== Stages.error) {
+        setStage(Stages.error);
+        return;
+      }
+    }
+    if (!isLoading && !isRefetching && blockNumber) {
+      if (stage !== Stages.show) {
+        setStage(Stages.show);
+        return;
+      }
+    }
+
+    return () => {};
+  }, [stage, blockNumber, isLoading, isRefetching, error]);
+
+  return (
+    <TooltipController content="Latest block mined every 12 secs" side="bottom">
+      <div className="flex space-x-1 items-center border rounded-lg p-1 dark:hover:bg-neutral-800">
+        <div
+          className={`w-3 h-3 ${
+            isStale ? "bg-yellow-500" : "bg-green-500"
+          } rounded-full`}
+        />
+        {stage === Stages.loading ? <Loading /> : null}
+        {stage === Stages.error && error ? <ErrorData error={error} /> : null}
+        {stage === Stages.show && blockNumber ? (
           <Link
             className="text-blue-400 text-sm"
             href={`/block/${blockNumber}`}
           >
             {blockNumber}
           </Link>
-        </div>
-      </TooltipController>
-    );
+        ) : null}
+      </div>
+    </TooltipController>
+  );
 };
 
 const ErrorData: React.FC<{ error: Error }> = ({ error }) => {
