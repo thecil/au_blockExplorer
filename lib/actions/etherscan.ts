62f3056f75aa419b3b67365ad070a4118f920e1b
thecil <thecilsurf@gmail.com>
2025-01-13 17:13:45 -0400

    feat: contract-query

--- lib/actions/etherscan.ts
+++ lib/actions/etherscan.ts
@@ -1,5 +1,8 @@
 "use server";
 
+import { contractAbiResponse } from "@/types/etherscan";
+import { Web3Address } from "@/types/web3";
+
 const ETHERSCAN_API_KEY = process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY;
 
 // Returns the ether price
@@ -17,6 +20,26 @@ export const getEthPrice = async () => {
     return JSON.stringify(data);
   } catch (error) {
     console.error(`getEthPrice: ${error}`);
+    throw error;
+  }
+};
+
+// Returns the Contract Application Binary Interface ( ABI ) of a verified smart contract.
+export const getContractAbi = async (address: Web3Address) => {
+  if (!address) return;
+  try {
+    const response = await fetch(
+      `https://api.etherscan.io/v2/api?chainid=1&module=contract&action=getabi&address=${address}&apikey=${ETHERSCAN_API_KEY}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data: contractAbiResponse = await response.json();
+    return data;
+  } catch (error) {
+    console.error(`getContractAbi: ${error}`);
     throw error;
   }
 };
---
+++ types/etherscan.ts
@@ -0,0 +1,5 @@
+export type contractAbiResponse = {
+  status: number;
+  message: string;
+  result: [];
+};
